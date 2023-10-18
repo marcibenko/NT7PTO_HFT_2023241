d692e1466d144a94ef5edceda11e6ea001f49c9a
Márton <Mrton@EGYEDI-NEV>
2023-10-18 12:26:59 +0200

    Add project files.

---
+++ NT7PTO_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NT7PTO_HFT_2023241.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
