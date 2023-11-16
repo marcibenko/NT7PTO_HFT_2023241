70cbeb9177c1a64c8a6e8d9029faa9178b53a89a
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-16 16:47:48 +0100

    Created spaceship repo class

---
+++ NT7PTO_HFT_2023241.Repository/ModelRepositories/SpaceshipRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using NT7PTO_HFT_2023241.Models;
+
+namespace NT7PTO_HFT_2023241.Repository
+{
+    public class SpaceshipRepository: Repository<Spaceship>, IRepository<Spaceship>
+    {
+        public SpaceshipRepository(SpaceTravelsDbContext ctx): base(ctx)
+        {
+
+        }
+
+        public override Spaceship Read(string id)
+        {
+            return ctx.Spaceships.FirstOrDefault(s => s.spaceshipId == id);
+        }
+
+        public override void Update(Spaceship item)
+        {
+            var old = Read(item.spaceshipId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
+
