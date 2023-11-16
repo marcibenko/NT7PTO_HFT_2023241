70acea38d4112a9ba96128803f220213d87915fc
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-16 16:45:07 +0100

    Created Repository class

---
+++ NT7PTO_HFT_2023241.Repository/GenericRepository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using NT7PTO_HFT_2023241.Repository;
+using System;
+using System.Linq;
+
+namespace NT7PTO_HFT_2023241.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected SpaceTravelsDbContext ctx;
+
+        public Repository(SpaceTravelsDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract T Read(string id);
+        public abstract void Update(T item);
+        
+    }
+}
+
