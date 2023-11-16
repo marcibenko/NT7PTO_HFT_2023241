608c4f8908e0cb1e4a50aa366d85753a699e8059
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-16 16:48:01 +0100

    Created spacetravel repo class

---
+++ NT7PTO_HFT_2023241.Repository/ModelRepositories/SpaceTravelRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using NT7PTO_HFT_2023241.Models;
+
+namespace NT7PTO_HFT_2023241.Repository
+{
+    public class SpaceTravelRepository : Repository<SpaceTravel>, IRepository<SpaceTravel>
+    {
+        public SpaceTravelRepository(SpaceTravelsDbContext ctx): base(ctx)
+        {
+
+        }
+
+        public override SpaceTravel Read(string id)
+        {
+            return ctx.SpaceTravels.FirstOrDefault(s => s.travelId == id);
+        }
+
+        public override void Update(SpaceTravel item)
+        {
+            var old = Read(item.travelId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
