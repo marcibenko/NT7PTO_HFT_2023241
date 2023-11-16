fa441fbce07475b7f3c98699d9fb238d14ef3de6
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-16 16:45:28 +0100

    Created Repository interface

---
+++ NT7PTO_HFT_2023241.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT7PTO_HFT_2023241.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(string id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(string id);
+    }
+}
