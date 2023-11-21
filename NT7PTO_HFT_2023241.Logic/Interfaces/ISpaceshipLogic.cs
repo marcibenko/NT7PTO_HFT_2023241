fa9f3700796a184b3830c21bad2bb890581b4f6a
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-21 22:13:36 +0100

    Added Spaceship Logic interface

---
+++ NT7PTO_HFT_2023241.Logic/Interfaces/ISpaceshipLogic.cs
@@ -0,0 +1,31 @@
+﻿using NT7PTO_HFT_2023241.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NT7PTO_HFT_2023241.Logic
+{
+    public interface ISpaceshipLogic
+    {
+        void Create(Spaceship item);
+        void Delete(string id);
+        Spaceship Read(string id);
+        IQueryable<Spaceship> ReadAll();
+        void Update(Spaceship item);
+
+        //non cruds
+
+        //returns a list of captains and their number of ships sorted descending by their number of ships
+        IEnumerable<SpaceshipLogic.CapAndShips> CaptainsAndShips();
+
+
+        //returns a list of ships, which havent been used
+        IEnumerable<Spaceship> UnusedShips();
+
+
+
+
+
+        
+    }
+}
