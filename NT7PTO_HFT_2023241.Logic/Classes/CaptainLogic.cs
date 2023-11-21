349a1d317ca78a61a3617b777bbc606eb7044f5a
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-21 22:12:59 +0100

    Added Captain Logic class

---
+++ NT7PTO_HFT_2023241.Logic/Classes/CaptainLogic.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NT7PTO_HFT_2023241.Models;
+using NT7PTO_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace NT7PTO_HFT_2023241.Logic
+{
+    public class CaptainLogic : ICaptainLogic
+    {
+        IRepository<Captain> repo;
+
+        public CaptainLogic(IRepository<Captain> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Captain item)
+        {
+            this.repo.Create(item);          
+        }
+
+        public void Delete(string id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Captain Read(string id)
+        {
+            var cap = this.repo.Read(id);
+            if(cap == null)
+            {
+                throw new ArgumentException("Captain not exists");
+            }
+            return cap;
+        }
+
+        public IQueryable<Captain> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Captain item)
+        {
+            this.repo.Update(item);
+        }
+
+        //non cruds
+        public IEnumerable<Captain> MostDangerousCaptains()
+        {
+            return this.repo
+                           .ReadAll()
+                           .Include(t => t.Spaceships)
+                           .Select(t => new
+                            {
+                             key = t,
+                             rating = t.Spaceships.Sum(s => s.size * (int)s.type)
+                            })
+                           .OrderByDescending(t => t.rating)
+                           .Select(t => t.key);
+        }
+
+        public IEnumerable<Captain> BiggestShip()
+        {
+            return this.repo
+                        .ReadAll()
+                        .Include(t => t.Spaceships)
+                        .OrderByDescending(t => t.Spaceships.Count());
+        }
+    }
+}
