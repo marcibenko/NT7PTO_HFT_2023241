1776322c10ebfa1d951200905b555c56715a73fd
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-21 22:13:52 +0100

    Added SpaceTravel interface

---
+++ NT7PTO_HFT_2023241.Logic/Interfaces/ISpaceTravelLogic.cs
@@ -0,0 +1,21 @@
+﻿using NT7PTO_HFT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NT7PTO_HFT_2023241.Logic
+{
+    public interface ISpaceTravelLogic
+    {
+        void Create(SpaceTravel item);
+        void Delete(string id);
+        SpaceTravel Read(string id);
+        IQueryable<SpaceTravel> ReadAll();
+        void Update(SpaceTravel item);
+
+        //non cruds
+
+        //returns a list of captains and their number of travels, sorted ascending by travels
+        IEnumerable<SpaceTravelLogic.CaptainsTravels> MostTravels();
+
+    }
+}
