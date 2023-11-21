cff11c8b3c851879d59c5abd32023f508c2c4cee
Márton <marton.benko@stud.uni-obuda.hu>
2023-11-21 22:13:15 +0100

    Added Captain Logic interface

---
+++ NT7PTO_HFT_2023241.Logic/Interfaces/ICaptainLogic.cs
@@ -0,0 +1,27 @@
+﻿using NT7PTO_HFT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NT7PTO_HFT_2023241.Logic
+{
+    public interface ICaptainLogic
+    {
+        void Create(Captain item);
+        void Delete(string id);
+        Captain Read(string id);
+        IQueryable<Captain> ReadAll();
+        void Update(Captain item);
+
+        //non crud
+
+        ////returns a list of captains, sorted ascending by their number of travels 
+        //IEnumerable<Captain> MostTravels();
+
+        //returns a list of captains, sorted descending by their level of threat
+        //formula is (spaceshipType * size) summed for all their ships
+        IEnumerable<Captain> MostDangerousCaptains();
+
+        //returns a list of captains, sorted descending by their max ship size
+        IEnumerable<Captain> BiggestShip();
+    }
+}
