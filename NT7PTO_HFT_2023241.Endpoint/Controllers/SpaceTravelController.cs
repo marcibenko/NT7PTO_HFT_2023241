2579312a78f131d459b25163a3518f42e9a025c4
Márton <marton.benko@stud.uni-obuda.hu>
2023-12-06 19:08:08 +0100

    created controller class

---
+++ NT7PTO_HFT_2023241.Endpoint/Controllers/SpaceTravelController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NT7PTO_HFT_2023241.Logic;
+using NT7PTO_HFT_2023241.Models;
+using System.Collections.Generic;
+
+namespace NT7PTO_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    public class SpaceTravelController: ControllerBase
+    {
+        ISpaceTravelLogic logic;
+
+        public SpaceTravelController(ISpaceTravelLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<SpaceTravel> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public SpaceTravel Read(string id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] SpaceTravel value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] SpaceTravel value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
