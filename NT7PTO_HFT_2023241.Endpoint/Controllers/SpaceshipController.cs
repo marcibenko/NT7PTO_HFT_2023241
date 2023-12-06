ee595028bfda222d13e7f3518d957e35dff6b73c
Márton <marton.benko@stud.uni-obuda.hu>
2023-12-06 19:08:04 +0100

    created controller class

---
+++ NT7PTO_HFT_2023241.Endpoint/Controllers/SpaceshipController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NT7PTO_HFT_2023241.Logic;
+using NT7PTO_HFT_2023241.Models;
+using System.Collections.Generic;
+
+namespace NT7PTO_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    public class SpaceshipController: ControllerBase
+    {
+        ISpaceshipLogic logic;
+
+        public SpaceshipController(ISpaceshipLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Spaceship> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Spaceship Read(string id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Spaceship value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Spaceship value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
